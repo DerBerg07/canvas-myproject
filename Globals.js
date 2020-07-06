63ff4ee8b10ee089deddde99f44aa3ecdf6fcd46
Oleg <34315653+DerBerg07@users.noreply.github.com>
2020-07-06 03:54:16 +0300

    test

--- Globals.js
+++
@@ -1,5 +0,0 @@
-let width = document.getElementById("container").offsetWidth;
-let height = document.getElementById("container").offsetHeight;
-let newPosGlobal = {x: 0, y: 0};
-let newScaleGlobal = 1;
-let linedrag = false;
